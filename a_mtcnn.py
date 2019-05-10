474896c07c6e7ef2171ca1c42c87a3a4f35542b7
thwfhk <thwfhk@163.com>
2019-05-10 19:34:13 +0800

    3ddfa

--- a_mtcnn.py
+++ a_mtcnn.py
@@ -1,5 +1,6 @@
 #%%
 import mtcnn
+from MEOW3DDFA import a_3ddfa
 import filters
 import face_recognition as fr
 import dlib
@@ -29,13 +30,19 @@ def get_landmarks(img, predictor = predictor5): #in rgb
     floc, fslands = mtcnn.detect_faces(Image.fromarray(img), min_face_size=100)
     face_locations = []
     faces_landmarks = []
+    faceRects = []
     for (x1, y1, x2, y2, p) in floc:
         face_locations.append(list(map(int, (y1, x2, y2, x1))))
-        # dlib
+        faceRects.append(dlib.rectangle(int(x1), int(y1), int(x2), int(y2)))
+        ''' dlib
         faceRect = dlib.rectangle(int(x1), int(y1), int(x2), int(y2))
         shape = predictor(img, faceRect)
         face_landmarks = face_utils.shape_to_np(shape)
         faces_landmarks.append(face_landmarks)
+        '''
+
+    # NOTE: using 3ddfa
+    faces_landmarks, Ps, poses = a_3ddfa.meow_landmarks(img, faceRects)
 
     ''' mtcnn
     for flands in fslands:
@@ -44,23 +51,27 @@ def get_landmarks(img, predictor = predictor5): #in rgb
             face_landmarks.append((int(flands[x]), int(flands[x+5])))
         faces_landmarks.append(face_landmarks)
     '''
-    # fr
-    # faces_landmarks = fr.face_landmarks(img, face_locations, model='large')
+    ''' fr
+    faces_landmarks = fr.face_landmarks(img, face_locations, model='large')
+    '''
     return face_locations, faces_landmarks
 
 
 @get_time
 def plot(img, face_locations, faces_landmarks):
-    for (top, right, bottom, left), face_landmarks in zip(face_locations, faces_landmarks):
+    for (top, right, bottom, left), points in zip(face_locations, faces_landmarks):
         cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-        #for landmark in face_landmarks.values():
-            #for i, (x, y) in enumerate(landmark):
-        for (x, y) in face_landmarks:
-            #font = cv2.FONT_HERSHEY_SIMPLEX
-            #cv2.putText(img, str(i),(x, y), font, 4, (255,255,255),2,cv2.LINE_AA)
+        for i in range(points.shape[1]):
+            x, y, z = points[0, i], points[1, i], points[2, i]
             cv2.circle(img, (x, y), 2, (255, 255, 255), 2)
-        # img = filters.wear_glass(img.copy(), face_landmarks, "glass")
+        ''' dlib
+        for (x, y) in points:
+            #cv2.putText(img, str(i),(x, y), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255),2,cv2.LINE_AA)
+            cv2.circle(img, (x, y), 2, (255, 255, 255), 2)
+        '''
+        #img = filters.wear_glass(img.copy(), points, "glass")
     return img
+
 @get_time
 def plot_landmarks(img): #in rgb
     face_locations, faces_landmarks = get_landmarks(img)
@@ -69,10 +80,10 @@ def plot_landmarks(img): #in rgb
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    img1 = plt.imread("twh.jpg")
+    img1 = plt.imread("t.jpg")
     plt.imshow(plot_landmarks(img1))
     plt.show()
 
-    img2 = plt.imread("a.jpg")
+    img2 = plt.imread("s.jpg")
     plt.imshow(plot_landmarks(img2))
     plt.show()
